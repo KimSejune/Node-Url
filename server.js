396f5847ed2cacce76ab89a6be1f9a4956c14566
kimsejune <tpwns1088@gmail.com>
2017-08-30 15:26:49 +0900

    로깅인증설정

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
+const basicAuth = require('express-basic-auth')
+
 const app = express()
 
 app.set('view engine', 'ejs')
+// public안에 있는 파일들을 서버에서 static으로 제공하겠다.
+app.use('/static', express.static('public'))
+app.use(morgan('tiny'))
+
+app.use(basicAuth({
+  users: { 'admin': 'admin' },
+  challenge: true,
+  realm: 'Imb4T3st4pp'
+}))
 
 app.get('/', (req, res) => {
   // res.send('hi~~ 수현 and 세준 eating 닭발')
