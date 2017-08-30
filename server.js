94a911bfeb32daad33cbdf938b1412bd820ab113
kimsejune <tpwns1088@gmail.com>
2017-08-30 16:52:22 +0900

    미들웨어 수정
    app.use에 넣엇던 값들을 middleware로 변경

--- server.js
+++ server.js
@@ -16,20 +16,22 @@ const data = [
 
 const app = express()
 
+const authMiddleware = basicAuth({
+  users: { 'admin': 'admin' },
+  challenge: true,
+  realm: 'Imb4T3st4pp'
+})
+
+const bodyParserMiddleWare = bodyParser.urlencoded({ extended: false })
+
 app.set('view engine', 'ejs')
 // public안에 있는 파일들을 서버에서 static으로 제공하겠다.
 app.use('/static', express.static('public'))
 app.use(morgan('tiny'))
-// parse application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(basicAuth({
-  users: { 'admin': 'admin' },
-  challenge: true,
-  realm: 'Imb4T3st4pp'
-}))
+
  
-app.get('/', (req, res) => {
+app.get('/', authMiddleware, (req, res) => {
   // res.send('hi~~ 수현 and 세준 eating 닭발')
   res.render('index.ejs', {data})
   
@@ -48,7 +50,7 @@ app.get('/:id', (req, res) => {
 
 })
 
-app.post('/', (req, res) => {
+app.post('/', authMiddleware, bodyParserMiddleWare, (req, res) => {
   // 값의 제출을 누르면 이곳에 모인다.
   const longUrl = req.body.longUrl
   let id
