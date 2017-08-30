cee3dc0714fbba396dc94328deb3c012c9c5f94c
kimsejune <tpwns1088@gmail.com>
2017-08-30 15:41:55 +0900

    데이터 구조 추가

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require('express')
 const morgan = require('morgan')
 const basicAuth = require('express-basic-auth')
 
+// http://localhost:3000/58dx37 이런식으로 shortUrl이 생성
+// 접속시 302가오고 
+const data = [
+  {
+    longUrl: 'http://google.com', id: '58DX37'
+  }
+]
+
+
 const app = express()
 
 app.set('view engine', 'ejs')
 // public안에 있는 파일들을 서버에서 static으로 제공하겠다.
 app.use('/static', express.static('public'))
 app.use(morgan('tiny'))
+
+
 
 app.use(basicAuth({
   users: { 'admin': 'admin' },
@@ -17,7 +28,7 @@ app.use(basicAuth({
 
 app.get('/', (req, res) => {
   // res.send('hi~~ 수현 and 세준 eating 닭발')
-  res.render('index.ejs')
+  res.render('index.ejs', {data})
   
 })
 
